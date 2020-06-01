e79d2fad77b691882a06c20e3943780fbec80a92
shiju <shiju@LAPTOP-QFQM3362>
2020-06-01 09:50:54 +0530

    Eureka Client

---
+++ aws_eureka/eurekaclient/eureka/src/main/java/com/efreelearn/eureka/GreetingController.java
@@ -0,0 +1,8 @@
+package com.efreelearn.eureka;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public interface GreetingController {
+    @RequestMapping("/greeting")
+    String greeting();
+}
---
+++ aws_eureka/eurekaclient/eureka/src/test/java/com/efreelearn/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efreelearn.eureka;
+
+ 
+import org.springframework.boot.test.context.SpringBootTest;
+
+ 
+class EurekaApplicationTests {
+
+ 
+	void contextLoads() {
+	}
+
+}
